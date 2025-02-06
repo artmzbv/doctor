9f47be9675abb318c72b4a4eec2413716f693b8b
Artem Zubov <artemzubov@Artems-MacBook-Pro.local>
2025-02-06 17:16:22 +0300

    help_phone

--- frontend/src/components/Help/Help.jsx
+++ frontend/src/components/Help/Help.jsx
@@ -11,7 +11,7 @@ const Help = () => {
           <h1 className='help__title'>{`ЧЕМ Я МОГУ ВАМ ПОМОЧЬ?`}</h1>
           <div className='help__card-container'>
           {services.map((service) =>
-          <div className='help___card' key={service.alt}>
+          <div className='help__card' key={service.alt}>
         <Link to={service.link} target="_blank" className='help__card'>
         <div className="help__image-container">
         <img className={`help__image  ${hover ? 'help__image_hover' : null}`}
