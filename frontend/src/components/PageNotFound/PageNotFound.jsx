f53c69fa9ca8a2e840da7f1d6758c9b31817d1df
Artem Zubov <artemzubov@Artems-MacBook-Pro.local>
2025-02-06 17:45:05 +0300

    not_found

--- frontend/src/components/PageNotFound/PageNotFound.jsx
+++ frontend/src/components/PageNotFound/PageNotFound.jsx
@@ -5,11 +5,11 @@ import './PageNotFound.css';
 export default function PageNotFound () {
     return (
       <section className="not-found">
-        <h1 className="not-found__title">{`Oops!\nPage Not Found`}</h1>
+        <h1 className="not-found__title">{`Страница\nне найдена`}</h1>
         <img className="not-found__image" src={''} alt=""/>
         <Link to="/">
             <button className="not-found__button">
-            <p className="not-found__text">Go to the Homepage</p>
+            <p className="not-found__text">На главную страницу</p>
             </button>
         </Link>
       </section>
